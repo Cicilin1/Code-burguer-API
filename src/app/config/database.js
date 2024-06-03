3cd88ea64435753ab5a3869888f07d1e7c94622a
Cicilini <guilherme.cicilini@hotmail.com>
2024-06-02 23:32:05 -0300

    config: Init PostgreSQL

---
+++ src/app/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'backburge645',
+  database: 'backburge',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
