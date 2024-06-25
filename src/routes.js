c4ceb79b94920fe7e114ae5104816eda00345492
Cicilini <guilherme.cicilini@hotmail.com>
2024-06-24 22:18:26 -0300

    feat(categories): Add routes, controller, and model for categories

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multerConfig';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductsController from './app/controllers/ProductsController';
+import CategoryController from './app/controllers/CategoryController';
 import authMiddlewares from './middlewares/auth';
 
 const routes = new Router();
@@ -16,5 +17,7 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddlewares);
 routes.post('/products', upload.single('file'), ProductsController.store);
 routes.get('/products', ProductsController.index);
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index);
 
 export default routes;
