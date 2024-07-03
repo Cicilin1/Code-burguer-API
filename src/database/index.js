74bc34162cf6c89c970d3d973ded7f890e47eee3
Cicilini <guilherme.cicilini@hotmail.com>
2024-07-02 22:15:35 -0300

    feat:connection with mongo container

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 
+import mongoose from 'mongoose';
 import configDatabase from '../config/database';
 
 import User from '../app/models/User';
@@ -11,6 +12,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +22,12 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburge',
+    );
   }
 }
 
